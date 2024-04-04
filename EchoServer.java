100ff99dc98ceab13ceecc3fe4c2a2492be799e3
Hossein <mh.amiri.notash@gmail.com>
2024-04-04 17:08:47 +0330

    Now it is possible for the client to take a string as input from the keyboard and send it to the server, and the server sends it back to the client.

--- EchoServer.java
+++ EchoServer.java
@@ -1,15 +1,20 @@
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.*;
 
 public class EchoServer {
     public static void main(String[] args) {
-        try{
-        System.out.println("Waiting for clients...");
-        ServerSocket ss = new ServerSocket(9086);
-        Socket soc = ss.accept();
-        System.out.println("Connection Established");
-        }catch(Exception e){
+        try {
+            System.out.println("Waiting for clients...");
+            ServerSocket ss = new ServerSocket(9086);
+            Socket soc = ss.accept();
+            System.out.println("Connection Established");
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            String str = in.readLine();
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+            out.println("Server Says : " + str);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    }//main
-}//class
+    }// main
+}// class
